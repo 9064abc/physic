1618f76985c23d49e2a9faab9a716911c7c8204d
9064abc <163479544+9064abc@users.noreply.github.com>
2024-11-08 21:27:51 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 console.log("Hello World main.js");
 console.log(CalVec([1,1],"+",[4,3]));
+const cvs = document.getElementById('canvas'); 
+const ctx = cvs.getContext("2d");
+ctx.translate(0,cvs.height); 
+ctx.scale(1,-1)
